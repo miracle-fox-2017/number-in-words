44cd4e218c747d7af0a093f31f4f5069fd7aa331
Angga Priambada <angga.idabagus@gmail.com>
2017-10-16 17:38:44 +0700

    NiW recursive

--- number_in_words.js
+++ number_in_words.js
@@ -1,9 +1,50 @@
 function numberToWords(number) {
   // Your code here
-}
+  var angka = ["", 'satu','dua','tiga','empat','lima','enam','tujuh','delapan','sembilan','sepuluh','sebelas'];
 
-// Driver code
-console.log(numberToWords(1000000));
+
+  if(number <= 11){
+    return angka[number];
+  }
+  else if(number <= 19 ){
+    return numberToWords(number % 10) + ' belas';
+  }
+  else if (number <= 99){
+    return numberToWords(Math.floor(number / 10)) + ' puluh ' + numberToWords(number % 10);
+  }
+  else if (number < 200){
+    return 'seratus ' + numberToWords(number % 100);
+  }
+  else if (number <= 999){
+      return numberToWords(Math.floor(number / 100)) + ' ratus ' + numberToWords(number % 100);
+  }
+  else if ( number < 2000){
+      return 'seribu ' + numberToWords(number % 1000);
+  }
+  else if ( number <= 99999){
+      return numberToWords(Math.floor(number / 1000)) + 'ribu ' + numberToWords(number % 1000);
+  }
+  
+  else if (number <= 999999){
+      return numberToWords(Math.floor(number / 1000)) + ' ribu ' + numberToWords(number % 1000);
+  }
+  else if (number < 99999999){
+      return numberToWords(Math.floor(number / 1000000)) + ' juta ' + numberToWords(number % 1000000);
+  }
+  else if (number < 200000000){
+      return "seratus " + numberToWords(number % 100000000)
+  }
+  else if (number <= 999999999){
+      return numberToWords(Math.floor(number / 1000000)) + ' juta ' + numberToWords(number % 1000000)
+  }
+  else if (number <= 999999999999){
+      return numberToWords(Math.floor(number / 1000000000)) + ' milyar ' + numberToWords (number % 1000000000)
+  }
+  else if (number <= 999999999999999){
+      return numberToWords(Math.floor(number / 10000000000)) + ' triliun ' + numberToWords(number % 10000000000);
+  }
+}
+console.log(numberToWords(1001001))
 
 module.exports = {
   numberToWords: numberToWords
