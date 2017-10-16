6d5ee89beabb1d497b1df41dd952042dab51ad23
trijadoel <triamri@gmail.com>
2017-10-16 21:55:12 +0700

    1 hingga 999

--- number_in_words.js
+++ number_in_words.js
@@ -1,10 +1,36 @@
 function numberToWords(number) {
-  // Your code here
+  var satuan = ['', 'satu', 'dua', 'tiga', 'empat', 'lima', 'enam', 'tujuh', 'delapan', 'sembilan', 'sepuluh', 'sebelas'];
+  var simpan = '';
+  if (number < 12) {
+    simpan = satuan[number];
+  } else if (number < 20) {
+    simpan = numberToWords(number % 10) + 'belas';
+  } else if (number < 20) {
+    simpan = numberToWords(number % 10) + 'belas';
+  } else if (number < 100) {
+    var nilai = Math.floor(number / 10);
+    simpan = numberToWords(nilai) + ' puluh ' + satuan[number % 10];
+  } else if (number < 200) {
+    simpan = 'seratus ' + numberToWords(number - 100);
+  } else if (number < 1000) {
+    var nilai = Math.floor(number / 100);
+    simpan = numberToWords(nilai) + ' ratus ' + numberToWords(number % 100);
+  }
+  return simpan;
 }
-
 // Driver code
-console.log(numberToWords(1000000));
+console.log(numberToWords(1));
+console.log(numberToWords(2));
+console.log(numberToWords(10));
+console.log(numberToWords(11));
+console.log(numberToWords(12));
+console.log(numberToWords(20));
+console.log(numberToWords(23));
+console.log(numberToWords(90));
+console.log(numberToWords(110));
+console.log(numberToWords(999));
+// console.log(numberToWords(1000000));
 
-module.exports = {
-  numberToWords: numberToWords
-}
+// module.exports = {
+//   numberToWords: numberToWords
+// }
