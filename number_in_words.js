f35646f1838f2f2cf77f85b85764a0ca39b73ab1
aditReza <lete.note3@gmail.com>
2017-10-16 16:51:00 +0700

    release 1

--- number_in_words.js
+++ number_in_words.js
@@ -1,9 +1,27 @@
 function numberToWords(number) {
   // Your code here
+  const words = ['','satu','dua','tiga','empat','lima','enam','tujuh','delapan','sembilan','sepuluh','sebelas']
+
+  if(number <= 11){
+    return words[number]
+  }else if(number <= 19){
+    return `${words[number-10]} belas`
+  }else if(number <= 99){
+    let split = number.toString().split('')
+    return `${words[split[0]]} puluh ${numberToWords(number%10)}`
+  }else if(number === 100){
+    return `seratus`
+  }else if(number <= 119){
+    return `seratus ${numberToWords(number%100)}`
+  }else if(number <= 999){
+    let split = number.toString().split('')
+    return `${words[split[0]]} ratus ${numberToWords(number%100)}`
+  }
+
 }
 
 // Driver code
-console.log(numberToWords(1000000));
+console.log(numberToWords(919));
 
 module.exports = {
   numberToWords: numberToWords
