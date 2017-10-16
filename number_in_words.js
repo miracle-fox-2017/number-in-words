45a8566e09d8dd0c28ec6b40dd1a97b963a72601
angrha <angrha@gmail.com>
2017-10-16 22:46:06 +0700

    first commit, masih jauh dan bingung

--- number_in_words.js
+++ number_in_words.js
@@ -1,10 +1,31 @@
 function numberToWords(number) {
-  // Your code here
+  var key = ['','satu', 'dua', 'tiga', 'empat', 'lima', 'enam', 'tujuh', 'delapan', 'sembilan', 'sepuluh',
+   'sebelas', 'dua belas', 'tiga belas', 'empat belas', 'lima belas', 'enam belas', 'tujuh belas', 'delapan belas',
+   'sembilan belas']
+
+  if(number < 20){
+    return key[number]
+  }
+  else if(number < 100){
+    // var angka = Math.floor(number/10)
+    return numberToWords(Math.floor(number/10)) + ' puluh ' + numberToWords(number % 10)
+  }
+  else if(number < 200){
+    return 'seratus ' + numberToWords(number %100)
+  }
+  else if(number < 1000){
+    // var angka = Math.floor(number/10)
+    return numberToWords(Math.floor(number / 100)) + ' ratus ' + numberToWords(number % 100)
+   }
+  // else if (number)
 }
 
 // Driver code
-console.log(numberToWords(1000000));
-
-module.exports = {
-  numberToWords: numberToWords
-}
+console.log(numberToWords(4)); //"empat"
+console.log(numberToWords(17)); //"sepuluh tujuh"
+console.log(numberToWords(27)); //"dua puluh tujuh"
+console.log(numberToWords(144)); //
+console.log(numberToWords(911)); //
+// console.log(numberToWords(270000)); //
+// console.log(numberToWords(5500000)); //
+// console.log(numberToWords(1000000));
